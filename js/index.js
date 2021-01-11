bdfd6e123381d6137e043354fbd16deb017b562b
oezge sebisteri <sebisteri@outlook.com>
2021-01-11 18:37:38 +0100

    adds correction to 3.2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-// Iteration 1: Names and Input
 console.log("Hello");
 
+//Iteration 1
 let hacker1
 hacker1 = "Oezge"
 console.log(`The driver's name is ${hacker1}.`)
@@ -10,18 +10,18 @@ hacker2 = "Oriane"
 console.log(`The navigator's name is ${hacker2}.`)
 
 
-// Iteration 2: Conditionals
+console.log(hacker2.length);
+
+
 //Iteration 2
 if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
-    //
-  } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-  } else if (hacker1.length === hacker2.length) {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
-  }
-
-// Iteration 3: Loops
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  //
+} else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else if (hacker1.length === hacker2.length) {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+}
 
 //3.1
 let i=0;
@@ -35,8 +35,13 @@ console.log(stringName.toUpperCase())
 //3.2
 let y;
 let stringName2 = ''
-for(y=hacker1.length; y >= 0; y--) {
+for(y=hacker1.length; y >= null; y--) {
   stringName2 = stringName2 + hacker2[y]
   }
 
 console.log(stringName2)
+
+
+
+
+
