684c382a9136863fa3881d4d74bd1fe8e40663c6
oezge sebisteri <sebisteri@outlook.com>
2021-01-11 17:27:27 +0100

    adds first iteration

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 // Iteration 1: Names and Input
+console.log("Hello");
+
+let hacker1
+hacker1 = "Oezge"
+console.log(`The driver's name is ${hacker1}.`)
+
+let hacker2;
+hacker2 = "Oriane"
+console.log(`The navigator's name is ${hacker2}.`)
+
+
+
 
 
 // Iteration 2: Conditionals
