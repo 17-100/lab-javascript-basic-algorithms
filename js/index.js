db0cdf694c683f78c2cd68dc4f3f854f1ecedbd0
Özge Sebisteri <sebisteri@outlook.com>
2022-11-22 21:30:31 +0100

    3.3 done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 // Iteration 1: Names and Input
-var hacker1 = "Leander German";
-var hacker2 = "Leo Alman";
+var hacker1 = "Zelli Gehrman";
+var hacker2 = "Stefan Alman";
 
 
 
-
-// Iteration 2: Conditionals
 /*
+// Iteration 2: Conditionals
 function longerName(driver, navigator) {
     var result;
     if (driver.length > navigator.length) {
@@ -21,14 +20,13 @@ function longerName(driver, navigator) {
 
     return result;
 }
-
-
 console.log(longerName(hacker1, hacker2));
 */
 
-
+/*
 // Iteration 3: Loops
-function driversName(driver) {
+// 3.1
+function nameCapital(driver) {
     var arr = driver.toUpperCase().split(" ");
     var driverName = [... arr[0]]
     var result = "";
@@ -37,5 +35,44 @@ function driversName(driver) {
     }
     return result;
 }
+console.log(nameCapital(hacker1));
 
-console.log(driversName(hacker1));
+// 3.2
+function nameReverse(navigator){
+    var arr = navigator.split(" ")
+    var name = arr[0]
+    var nameArr = [... name];
+    var nameArrReverse = nameArr.reverse();
+    var nameReverse = nameArrReverse.join("");
+
+    return nameReverse;
+};
+console.log(nameReverse(hacker2));
+
+
+// 3.3
+function lexicographicOrder(driver, navigator) {
+    var arr1 = driver.split(" ");
+    var arr2 = navigator.split(" ");
+    var driverName = arr1[0];
+    var naviName = arr2[0];
+
+    var bothNames = [driverName, naviName];
+    var sortedNames = bothNames.sort();
+
+    var result;
+    
+    if(sortedNames[0] == driverName) {
+        result = "The driver's name goes first.";
+    }
+    if(sortedNames[0] == naviName) {
+        result = "Yo, the navigator goes first definitely.";
+    }
+
+    return result;
+}
+console.log(lexicographicOrder(hacker1, hacker2));
+*/
+
+
+// Bonus
